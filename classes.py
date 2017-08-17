4e9a8fe9a8d74f729d75020bc5c09169e1d96dd5
drewbitt <andrew@drewbitt.com>
2017-08-17 16:05:41 -0400

    cleanup classes

--- classes.py
+++ classes.py
@@ -1,16 +1,23 @@
-import abc 		# for abstract methods. not really needed but eh
-import scrape	# debug
+"""
+To Do:
+	* Implement getting the image for each object
+	* Speed fixes
+	* Remove getters/setters
+	* Remove abc & abstract methods
+"""
+
+import abc 			# for abstract methods. not really needed but eh
+import scrape		# debug
 
 """ Class for the board that contains the board layout (objects in a list), the cycle of how the objects change,
-and the goal. Has method to change board when piece is passed to it at a certain position"""
-class Board(list):		# maybe subclass this as list or object (depending on approach)
+and the goal. Has method to change the board"""
+class Board(list):		# may not need to be of class list
 
 	def __init__(self, board_array, cycle_array):
 		super().__init__()
 		self.board_array = board_array
 		self.cycle_array = cycle_array
 		self.board_array = self.__convert_board(self.board_array)
-
 		# Convert cycle array to become a list of their corresponding object
 		self.cycle_array = self.__convert_cycle(self.cycle_array)
 		# Create dictionary of order of conversion
@@ -22,20 +29,17 @@ class Board(list):		# maybe subclass this as list or object (depending on approa
 			for index2, col in enumerate(row):
 				if col == "swo":
 					col = SwordObj([index, index2])		# row, col
-				elif col ==  "cro":
+				elif col == "cro":
 					col = CrownObj([index, index2])
 				elif col == "gob":
 					col = GobletObj([index, index2])
 				row[index2] = col
 		return b
 
-	"""Convert text values of cycle into objects of their own type
-	Note: uses default value of None for coordinates since that doesn't matter here
-	"""
+	"""Convert text values of cycle_array into objects. Uses default value of None for coordinate"""
 	def __convert_cycle(self, c):
 		for index, i in enumerate(c):
-			i = i[:-2]		# remove the trailing info from cycle_array items so you just get "cro", "swo" etc
-			# convert cycle_array items to objects as well
+			i = i[:-2]		# remove the trailing info from cycle_array elements
 			if i == "swo":
 				i = SwordObj()
 			elif i == "cro":
@@ -48,54 +52,45 @@ class Board(list):		# maybe subclass this as list or object (depending on approa
 	def get_cycle(self):
 		return self.cycle_array
 
-	"""Returns the goal (as an object), which is always the second to last item in cycle array
-	"""
 	def get_goal(self):
 		return self.cycle_array[-2]
 
-	"""Create dictionary of how the types should change"""
+	"""Create dictionary of how the object types should change"""
 	def __create_cycle_type_dict(self):
 		cycle_type_dict = {}
 		for index, i in enumerate(self.cycle_array[:-1]):
-			cycle_type_dict[ i.name() ] = self.cycle_array[index+1].name()
+			cycle_type_dict[i.name()] = self.cycle_array[index+1].name()
 		return cycle_type_dict
 
-	"""Change the type of a single obj. Updates position of new node to old obj's position as well
-	Is used when placing a shape onto the board"""
+	"""Change the type of a single obj. Updates position of new node"""
 	def __change_type(self, board_obj):
 		name = self.type_dict[board_obj.name()]
-		name = name + "Obj(board_obj.get_coord())"
+		name += "Obj(board_obj.get_coord())"
 		return eval(name)		# evaluates string to create object
 
 	""" coord is a list of all coordinates to change, chk=True means only check and return board after placing,
-	without actually writing to self.board_array.
-	Returns True if it ran sucessfully
+	without writing to self.board_array.
+	Returns True if sucessful & chk=False
 	"""
 	def place_shape(self, coord, chk=False):
 		board = self.board_array
 		for c in coord:
-			board[c[0]][c[1]] = self.__change_type( board [c[0]][c[1]] )
+			board[c[0]][c[1]] = self.__change_type(board[c[0]][c[1]])
 		if chk:
 			return board
-		board = self.board_array
+		self.board_array = board
 		return True
 
 # --------------Objects
 # Add others later once I get to their levels and figure out their names
-
-# Individual objects (crown, goblet, etc.) would be children to this
 class TypeOfObj(metaclass=abc.ABCMeta):
-
-	# Going to store coordinate to easily look at positions. May not be needed, but shouldn't be harmful.
-	# Store coord in list
+	# coord is list [row, col]
 	def __init__(self, coord=None):
 		self.coord = coord
-
 	def get_coord(self):
 		return self.coord
 	def update_coord(self, coord):
-		coord = self.coord
-
+		self.coord = coord
 	@abc.abstractmethod
 	def get_image(self):
 		"""Returns img for the class to be used for displaying"""
@@ -106,25 +101,25 @@ class TypeOfObj(metaclass=abc.ABCMeta):
 # Unsure if I want each class to actually print the image with .print or just return the filename
 class CrownObj(TypeOfObj):
 	def get_image(self):
-		return("IMAGE GOES HERE")
+		return "IMAGE GOES HERE"
 	def name(self):
-		return("Crown")
+		return "Crown"
 	def print(self):
-		print("Crown Object - Coord = {}".format(self.coord))
+		print("Crown Object, coord {}".format(self.coord))
 class GobletObj(TypeOfObj):
 	def get_image(self):
-		return("IMAGE GOES HERE")
+		return "IMAGE GOES HERE"
 	def name(self):
-		return("Goblet")
+		return "Goblet"
 	def print(self):
-		print("Goblet Object - Coord = {}".format(self.coord))
+		print("Goblet Object, coord {}".format(self.coord))
 class SwordObj(TypeOfObj):
 	def get_image(self):
-		return("IMAGE GOES HERE")
+		return "IMAGE GOES HERE"
 	def name(self):
-		return("Sword")
+		return "Sword"
 	def print(self):
-		print("Sword Object - Coord= {}".format(self.coord))
+		print("Sword Object, coord {}".format(self.coord))
 TypeOfObj.register(CrownObj)
 TypeOfObj.register(GobletObj)
 TypeOfObj.register(SwordObj)
@@ -138,14 +133,16 @@ class Pieces(list):
 
 	"""Remove all empty (all elements = 0) rows and columns to get true shape size"""
 	def __remove_empty(self, p):
-		"""Transpose goes through rows and columns. remove_blank_rows removes all rows where all elements eval to False,
-		which 0 does"""
+		# Transpose goes through rows and columns. remove_blank_rows removes all rows where all elements eval False,
+		# which 0 does
 		def transpose(a):
 			return zip(*a)
+
 		def remove_blank_rows(a):
 			return [list(row) for row in a if any(row)]
+
 		for index, i in enumerate(p):
-			i = remove_blank_rows( transpose(remove_blank_rows(transpose(i))))
+			i = remove_blank_rows(transpose(remove_blank_rows(transpose(i))))
 			p[index] = i
 		return p
 
@@ -155,7 +152,6 @@ class Pieces(list):
 		else:
 			return Piece(self.pieces_array[index])
 
-"""Class for an individual piece"""
 class Piece:
 	def __init__(self, layout):
 		self.layout = layout
@@ -163,11 +159,8 @@ class Piece:
 	def get_layout(self):
 		return self.layout
 
-	"""Will make method that can return image filenames (or print directly) of each block so that the piece
-	 can be easily displayed in GUI
-	 """
 	def print(self):
-		print("IMPLEMENT LATER")
+		return "IMPLEMENT LATER"
 
 '''
 b, c, p= scrape.get_all("C:/Users/andys-pc/Downloads/ShapeShifter.html")
