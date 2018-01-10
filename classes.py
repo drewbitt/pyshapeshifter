66f304a74b57b015f98d3c95543fc21671665e07
Andrew Bitterling <andrew@drewbitt.com>
2018-01-10 16:07:19 -0500

    quick style change

--- classes.py
+++ classes.py
@@ -1,148 +1,146 @@
-"""
+#!/usr/bin/python3
+'''
 To Do:
-	* Implement getting the image for each object
-	* Speed fixes
-"""
+    * Implement getting the image for each object
+    * Speed fixes
+'''
 
 import abc 			# not really needed but eh
 # import scrape		# debug
 
-""" Class for the board that contains the board layout (objects in a list), the cycle of how the objects change,
-and the goal. Has method to change the board"""
+''' Class for the board that contains the board layout (objects in a list), the cycle of how the objects change,
+and the goal. Has method to change the board'''
 class Board(list):		# may not need to be of class list
 
-	def __init__(self, board_array, cycle_array):
-		super().__init__()
-		self.board_array = board_array
-		self.cycle_array = cycle_array
-		self.board_array = self.__convert_board(self.board_array)
-		# Convert cycle array to become a list of their corresponding object
-		self.cycle_array = self.__convert_cycle(self.cycle_array)
-		# Create dictionary of order of conversion
-		self.type_dict = self.__create_cycle_type_dict()
+    def __init__(self, board_array, cycle_array):
+        super().__init__()
+        self.board_array = board_array
+        self.cycle_array = cycle_array
+        self.board_array = self.__convert_board(self.board_array)
+        # Convert cycle array to become a list of their corresponding object
+        self.cycle_array = self.__convert_cycle(self.cycle_array)
+        # Create dictionary of order of conversion
+        self.type_dict = self.__create_cycle_type_dict()
 
-	"""Convert text values of board (e.g. "swo") into objects of their type"""
-	def __convert_board(self, b):
-		for index, row in enumerate(b):
-			for index2, col in enumerate(row):
-				if col == "swo":
-					col = SwordObj([index, index2])		# row, col
-				elif col == "cro":
-					col = CrownObj([index, index2])
-				elif col == "gob":
-					col = GobletObj([index, index2])
-				row[index2] = col
-		return b
+    '''Convert text values of board (e.g. "swo") into objects of their type'''
+    def __convert_board(self, b):
+        for index, row in enumerate(b):
+            for index2, col in enumerate(row):
+                if col == "swo":
+                    col = SwordObj([index, index2])		# row, col
+                elif col == "cro":
+                    col = CrownObj([index, index2])
+                elif col == "gob":
+                    col = GobletObj([index, index2])
+                row[index2] = col
+        return b
 
-	"""Convert text values of cycle_array into objects. Uses default value of None for coordinate"""
-	def __convert_cycle(self, c):
-		for index, i in enumerate(c):
-			i = i[:-2]		# remove the trailing info from cycle_array elements
-			if i == "swo":
-				i = SwordObj()
-			elif i == "cro":
-				i = CrownObj()
-			elif i == "gob":
-				i = GobletObj()
-			c[index] = i
-		return c
+    '''Convert text values of cycle_array into objects. Uses default value of None for coordinate'''
+    def __convert_cycle(self, c):
+        for index, i in enumerate(c):
+            i = i[:-2]		# remove the trailing info from cycle_array elements
+            if i == "swo":
+                i = SwordObj()
+            elif i == "cro":
+                i = CrownObj()
+            elif i == "gob":
+                i = GobletObj()
+            c[index] = i
+        return c
 
-	def __create_cycle_type_dict(self):
-		cycle_type_dict = {}
-		for index, i in enumerate(self.cycle_array[:-1]):
-			cycle_type_dict[i.name()] = self.cycle_array[index+1].name()
-		return cycle_type_dict
+    def __create_cycle_type_dict(self):
+        cycle_type_dict = {}
+        for index, i in enumerate(self.cycle_array[:-1]):
+            cycle_type_dict[i.name()] = self.cycle_array[index+1].name()
+        return cycle_type_dict
 
-	def __change_type(self, board_obj):
-		name = self.type_dict[board_obj.name()]
-		name += "Obj(board_obj.get_coord())"
-		return eval(name)		# evaluates string to create object
+    def __change_type(self, board_obj):
+        name = self.type_dict[board_obj.name()]
+        name += "Obj(board_obj.get_coord())"
+        return eval(name)
 
-	""" coord is a list of all coordinates to change, chk=True means only check and return board after placing,
-	without writing to self.board_array.
-	Returns True if sucessful & chk=False
-	"""
-	def place_shape(self, coord, chk=False):
-		board = self.board_array
-		for c in coord:
-			board[c[0]][c[1]] = self.__change_type(board[c[0]][c[1]])
-		if chk:
-			return board
-		self.board_array = board
-		return True
+    ''' coord is a list of all coordinates to change, chk=True means only check and return board after placing,
+    without writing to self.board_array.
+    Returns True if sucessful & chk=False
+    '''
+    def place_shape(self, coord, chk=False):
+        board = self.board_array
+        for c in coord:
+            board[c[0]][c[1]] = self.__change_type(board[c[0]][c[1]])
+        if chk:
+            return board
+        self.board_array = board
+        return True
 
 # --------------Objects
 # Add others later once I get to their levels and figure out their names
 class TypeOfObj(metaclass=abc.ABCMeta):
-	# coord is list [row, col]
-	def __init__(self, coord=None):
-		self.coord = coord
-	@abc.abstractmethod
-	def get_image(self):
-		"""Returns img for the class to be used for displaying"""
-	@abc.abstractmethod
-	def print(self):
-		"""Print name of class and coordinate"""
+    '''Parent class of all objects'''
+    # coord is list [row, col]
+    def __init__(self, coord=None):
+        self.coord = coord
+    @abc.abstractmethod
+    def get_image(self):
+        '''Returns img for the class to be used for displaying'''
+    @abc.abstractmethod
+    def print(self):
+        '''Print name of class and coordinate'''
 
-# Unsure if I want each class to actually print the image with .print or just return the filename
 class CrownObj(TypeOfObj):
-	def get_image(self):
-		return "IMAGE GOES HERE"
-	def name(self):
-		return "Crown"
-	def print(self):
-		print("Crown Object, coord {}".format(self.coord))
+    name = "Crown"
+    def get_image(self):
+        return "IMAGE GOES HERE"
+    def print(self):
+        print("Crown Object, coord {}".format(self.coord))
 class GobletObj(TypeOfObj):
-	def get_image(self):
-		return "IMAGE GOES HERE"
-	def name(self):
-		return "Goblet"
-	def print(self):
-		print("Goblet Object, coord {}".format(self.coord))
+    name = "Goblet"
+    def get_image(self):
+        return "IMAGE GOES HERE"
+    def print(self):
+        print("Goblet Object, coord {}".format(self.coord))
 class SwordObj(TypeOfObj):
-	def get_image(self):
-		return "IMAGE GOES HERE"
-	def name(self):
-		return "Sword"
-	def print(self):
-		print("Sword Object, coord {}".format(self.coord))
-TypeOfObj.register(CrownObj)
+    name = "Sword"
+    def get_image(self):
+        return "IMAGE GOES HERE"
+    def print(self):
+        print("Sword Object, coord {}".format(self.coord))
+TypeOfObj.register(CrownObj)    # I know I can use ABC without doing this
 TypeOfObj.register(GobletObj)
 TypeOfObj.register(SwordObj)
 # -----------------------------------------------
-''' Simple list object of the pieces that slims down the array from extra 0s.'''
 class Pieces(list):
-	def __init__(self, pieces_array):
-		super().__init__()
-		self.pieces_array = pieces_array
-		self.pieces_array = self.__remove_empty(pieces_array)
+    ''' Simple list object of the pieces that slims down the array from extra 0s.'''
+    def __init__(self, pieces_array):
+        super().__init__()
+        self.pieces_array = pieces_array
+        self.pieces_array = self.__remove_empty(pieces_array)
 
-	"""Remove all empty (all elements = 0) rows and columns to get true shape size"""
-	def __remove_empty(self, p):
-		# Transpose goes through rows and columns. remove_blank_rows removes all rows where all elements eval False,
-		# which 0 does
-		def transpose(a):
-			return zip(*a)
+    def __remove_empty(self, p):
+        '''Remove all empty (all elements = 0) rows and columns to get true shape size'''
+        def transpose(a):
+            '''goes through rows and columns. remove_blank_rows removes all rows where all elements eval False,
+               which 0 does '''
+            return zip(*a)
 
-		def remove_blank_rows(a):
-			return [list(row) for row in a if any(row)]
+        def remove_blank_rows(a):
+            return [list(row) for row in a if any(row)]
 
-		for index, i in enumerate(p):
-			i = remove_blank_rows(transpose(remove_blank_rows(transpose(i))))
-			p[index] = i
-		return p
+        for index, i in enumerate(p):
+            i = remove_blank_rows(transpose(remove_blank_rows(transpose(i))))
+            p[index] = i
+        return p
 
-	def get_piece(self, index, remove=False):
-		if remove:
-			return Piece(self.pieces_array.pop(index))
-		else:
-			return Piece(self.pieces_array[index])
+    def get_piece(self, index, remove=False):
+        if remove:
+            return Piece(self.pieces_array.pop(index))
+        else:
+            return Piece(self.pieces_array[index])
 
 class Piece:
-	def __init__(self, layout):
-		self.layout = layout
-	def print(self):
-		return "IMPLEMENT LATER"
+    def __init__(self, layout):
+        self.layout = layout
+    def print(self):
+        return "IMPLEMENT LATER"
 
 '''
 b, c, p= scrape.get_all("C:/Users/andys-pc/Downloads/ShapeShifter.html")
--- scrape.py
+++ scrape.py
@@ -1,115 +1,108 @@
-""" File to scrape the .html file of the game's page to get layout, rules, and available shapes
+#!/usr/bin/python3
+''' File to scrape the .html file of the game's page to get layout, rules, and available shapes
 Could've been done much easier in Javascript, but I was exploring Python
-"""
+Uses HTMLParser for parsing shapes just so I could try it out instead of BeautifulSoup
+'''
 
 import sys
 from html.parser import HTMLParser
 
-""" Uses HTMLParser for parsing shapes just so I could try it out
-
-Overrides MyHTMLParser class methods handle_starttag and handle_endtag
-"""
-
 class MyHTMLParser(HTMLParser):
+    '''Class for parsing the shapeshifter page'''
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.size = [[0 for _ in range(5)] for _ in range(4)]		# max size = 4*5. array top-bottom, left-right
+        self.in_tr, self.in_td, self.in_correct_table = (False,)*3
+        self.count_row, self.count_cell = 0, 0
+        self.pieces_array = []
 
-	def __init__(self):
-		HTMLParser.__init__(self)
-		self.size = [[0 for _ in range(5)] for _ in range(4)]		# max size = 4*5. array top-bottom, left-right
-		self.in_tr, self.in_td, self.in_correct_table = (False,)*3
-		self.count_row, self.count_cell = 0, 0
-		self.pieces_array = []
+    def error(self, message):		# to implement all abstract methods
+        pass
 
-	def error(self, message):		# to implement all abstract methods
-		pass
+    def handle_starttag(self, tag, attrs):
+        if tag == 'table':
+            cellpadding = int(attrs[1][1])		# assumes cell_padding is always second, border first
+            # there is extra <td> & <tr> in a table b4 useful tags, so checking for correct table
+            if cellpadding == 0:		# incorrect table has cell_padding = 15 from what I've seen
+                self.in_correct_table = True
+        elif tag == 'tr':
+            if self.in_correct_table:
+                self.count_row += 1		# keeps track of what row we are in
+                self.in_tr = True
+        elif tag == 'td':
+            if self.in_correct_table:
+                self.count_cell += 1
+                self.in_td = True
+        elif tag == 'img':
+            # if an img is found, the img = one square block of the shape
+            # keep track of all blocks found in shape by making the found position = 1 in self.size
+            self.size[self.count_row-1][self.count_cell-1] += 1
 
-	def get_pieces_array(self):
-		return self.pieces_array
-
-	def handle_starttag(self, tag, attrs):
-		if tag == 'table':
-			cellpadding = int(attrs[1][1])		# assumes cell_padding is always second, border first
-			# there is extra <td> & <tr> in a table b4 useful tags, so checking for correct table
-			if cellpadding == 0:		# incorrect table has cell_padding = 15 from what I've seen
-				self.in_correct_table = True
-		elif tag == 'tr':
-			if self.in_correct_table:
-				self.count_row += 1		# keeps track of what row we are in
-				self.in_tr = True
-		elif tag == 'td':
-			if self.in_correct_table:
-				self.count_cell += 1
-				self.in_td = True
-		elif tag == 'img':
-			# if an img is found, the img = one square block of the shape
-			# keep track of all blocks found in shape by making the found position = 1 in self.size
-			self.size[self.count_row-1][self.count_cell-1] += 1
-
-	def handle_endtag(self, tag):
-		if self.in_correct_table:
-			if tag == 'td':
-				self.in_td = False
-			elif tag == 'tr':
-				self.in_tr = False
-				self.count_cell = 0
-			elif tag == 'table':
-				if self.in_correct_table:
-					self.in_correct_table = False
-					# store completed shape, reset shape size & all counts
-					self.pieces_array.append(self.size)
-					self.size = [[0 for _ in range(5)] for _ in range(4)]
-					self.count_row, self.count_cell = 0, 0
-
-"""Method for parsing the board, getting the cycle for changing types, and getting all pieces available
-All combined so that the file would not be iterated through multiple times
-
-Returns tuple of the board list, cycle list, and shape list"""
+    def handle_endtag(self, tag):
+        if self.in_correct_table:
+            if tag == 'td':
+                self.in_td = False
+            elif tag == 'tr':
+                self.in_tr = False
+                self.count_cell = 0
+            elif tag == 'table':
+                if self.in_correct_table:
+                    self.in_correct_table = False
+                    # store completed shape, reset shape size & all counts
+                    self.pieces_array.append(self.size)
+                    self.size = [[0 for _ in range(5)] for _ in range(4)]
+                    self.count_row, self.count_cell = 0, 0
 
 def get_all(infile):
-	board_array = []
-	cycle_array = []
-	pieces_array = []
-	try:
-		with open(infile, encoding='utf8') as f:
-			for line in f:
-				# 1a) Get board array
-				if'imgLocStr = new' in line:
-					# Board is an array. imgLocStr = new Array starts it, gives # rows
-					num_rows = int(line[line.index("(") + 1:line.index(")")])
-					# next line gives number of columns
-					line = next(f)
-					num_cols = int(line[line.index("(") + 1:line.index(")")])
-					# skip until line no longer contains = new Array. also create two-dimensional array
-					for _ in range(num_rows):
-						board_array.append([0] * num_rows)
-						line = next(f)
+    """Method for parsing the board, getting the cycle for changing types, and getting all pieces available
+    All combined so that the file would not be iterated through multiple times
 
-					# 1b) Fill board array
-					for col in range(num_cols):
-						for row in range(num_rows):
-							obj_type = line[line.index('"') + 1: len(line)-2]
-							board_array[row][col] = obj_type
-							line = next(f)
+    Returns tuple of the board list, cycle list, and shape list"""
+    board_array = []
+    cycle_array = []
+    pieces_array = []
+    try:
+        with open(infile, encoding='utf8') as f:
+            for line in f:
+                # 1a) Get board array
+                if'imgLocStr = new' in line:
+                    # Board is an array. imgLocStr = new Array starts it, gives # rows
+                    num_rows = int(line[line.index("(") + 1:line.index(")")])
+                    # next line gives number of columns
+                    line = next(f)
+                    num_cols = int(line[line.index("(") + 1:line.index(")")])
+                    # skip until line no longer contains = new Array. also create two-dimensional array
+                    for _ in range(num_rows):
+                        board_array.append([0] * num_rows)
+                        line = next(f)
 
-				# 2) Get cycle for changing types
-				# Goal is always second to last
-				if 'border="1" bordercolor="gray"' in line:
-					while "gif" in line:
-						# ignore all arrow images
-						if "arrow" not in line:
-							index_r = line.index(".gif")
-							index_l = line[:index_r].rfind("/")
-							cycle_array.append(line[index_l+1:index_r])
-						line = next(f)
+                    # 1b) Fill board array
+                    for col in range(num_cols):
+                        for row in range(num_rows):
+                            obj_type = line[line.index('"') + 1: len(line)-2]
+                            board_array[row][col] = obj_type
+                            line = next(f)
 
-				# 3) Get pieces available
-				if "<b><big>ACTIVE SHAPE</big></b>" in line:
-					parser = MyHTMLParser()
-					parser.feed(line)
-					pieces_array = parser.get_pieces_array()
+                # 2) Get cycle for changing types
+                # Goal is always second to last
+                if 'border="1" bordercolor="gray"' in line:
+                    while "gif" in line:
+                        # ignore all arrow images
+                        if "arrow" not in line:
+                            index_r = line.index(".gif")
+                            index_l = line[:index_r].rfind("/")
+                            cycle_array.append(line[index_l+1:index_r])
+                        line = next(f)
 
-		return (board_array, cycle_array, pieces_array)
-	except (OSError, IOError):		# python 3.3 IOError became OSError & FileNotFoundError is in OSError
-		sys.exit("Could not open file to scrape/parse")
+                # 3) Get pieces available
+                if "<b><big>ACTIVE SHAPE</big></b>" in line:
+                    parser = MyHTMLParser()
+                    parser.feed(line)
+                    pieces_array = parser.get_pieces_array()
+
+        return (board_array, cycle_array, pieces_array)
+    except (OSError, IOError):		# python 3.3 IOError became OSError & FileNotFoundError is in OSError
+        sys.exit("Could not open file to scrape/parse")
 
 if __name__ == '__main__':
-	get_all("C:/Users/andys-pc/Downloads/ShapeShifter.html")		# debugging
+    get_all("C:/Users/andys-pc/Downloads/ShapeShifter.html")		# debugging
